5ef8bc1988378f20e87713813e59ec2fb6673e5d
yuhi_deve <yuhinnn@gmail.com>
2016-05-25 16:43:33 +0900

    mod script.js

--- index10.php
+++ index10.php
@@ -12,9 +12,24 @@
 <li><a href="javascript:void(0);" class="c color">c</a></li>
 <button onclidk="getElement()">結果</button>
 
+
+<div class="q1">
 <input type="button" name="a" value="a" onClick="choice(1)">
 <input type="button" name="b" value="b" onClick="choice(2)">
 <input type="button" name="c" value="c" onClick="choice(3)">
+</div>
+
+<div class="q2">
+<input type="button" name="a" value="a" onClick="choice2(1)">
+<input type="button" name="b" value="b" onClick="choice2(2)">
+<input type="button" name="c" value="c" onClick="choice2(3)">
+</div>
+
+<div class="q3">
+<input type="button" name="a" value="a" onClick="choice(1)">
+<input type="button" name="b" value="b" onClick="choice(2)">
+<input type="button" name="c" value="c" onClick="choice(3)">
+</div>
 
     <script src="script.js"></script>
 
--- script.js
+++ script.js
@@ -3,6 +3,40 @@ var Q1 = 0 ;
 function choice(btn){
 if (btn  == 1){
 var Q1 = 1 ;
+console.log(Q1);
+return Q1;
+}
+else if(btn == 2){
+var Q1 = 2 ;
+console.log(Q1);
+return Q1;
+}else if(btn == 3){
+var Q1 = 3 ;
+console.log(Q1);
+return Q1;
+}
+}
+
+
+
+function choice2(btn){
+if (btn  == 1){
+var Q2 =choice(btn)+1 ;
+console.log(Q2)
+}
+else if(btn == 2){
+var Q2 =Q1+2 ;
+console.log(Q2)
+}else if(btn == 3){
+var Q2 =Q1+3 ;
+console.log(Q2)
+}
+}
+
+
+function choice(btn){
+if (btn  == 1){
+var Q1 = 1 ;
 console.log(Q1)
 }
 else if(btn == 2){
